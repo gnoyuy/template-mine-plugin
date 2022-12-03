37ee7a0f3a3aee68e17276b4c360a66de9035a21
GitHub Action <action@github.com>
2022-12-03 07:08:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/gnoyuy/templatemineplugin/MyBundle.kt
+++ src/main/kotlin/com/github/gnoyuy/templatemineplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gnoyuy.templatemineplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gnoyuy/templatemineplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gnoyuy/templatemineplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gnoyuy.templatemineplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gnoyuy.templatemineplugin.MyBundle
 
 class MyApplicationService {
 
